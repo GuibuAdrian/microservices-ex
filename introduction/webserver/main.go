a85fec4c33dd6d1b71f8380325b5dab5761f8c99
GuibuAdrian <guibuadrian@gmail.com>
2022-07-11 15:00:22 -0300

    Update message

--- introduction/webserver/main.go
+++ introduction/webserver/main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("Hello World"))
+		writer.Write([]byte("Hello World!"))
 	})
 
 	if err := http.ListenAndServe("localhost:8084", nil); err != nil { //Only works in the scope of this condition
