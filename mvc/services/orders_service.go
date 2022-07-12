a181add22a174fe55b7e2930117954d685a50532
AdriG <guibuadrian@gmail.com>
2022-07-12 12:45:03 -0300

    Feature/benchmark (#3)
    * Added benchmark example
    
    * Mock testing

--- mvc/services/orders_service.go
+++ mvc/services/orders_service.go
@@ -5,6 +5,16 @@ import (
 	"microservices-ex-app/mvc/utils"
 )
 
-func GetOrder(orderId int64) (*domain.Order, *utils.ApplicationError) {
-	return domain.GetOrder(orderId)
+type ordersService struct {
+}
+
+var OrdersService ordersService
+
+func (ordersSer *ordersService) GetOrder(orderId int64) (*domain.Order, *utils.ApplicationError) {
+	order, err := domain.OrderDao.GetOrder(orderId)
+	if err != nil {
+		return nil, err
+	}
+
+	return order, nil
 }
