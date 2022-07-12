f1b7286d2392e472073f3cbab041c8894cf19e57
GuibuAdrian <guibuadrian@gmail.com>
2022-07-12 17:23:57 -0300

    Deleted files from other branch

--- mvcGinGonic/app/url_mappings.go
+++
@@ -1,9 +0,0 @@
-package app
-
-import (
-	"microservices-ex-app/mvcGinGonic/controller"
-)
-
-func mapUrls() {
-	router.GET("/orders/:order_id", controller.GetOrder) //example: .../orders/123
-}
--- mvcGinGonic/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "microservices-ex-app/mvc/app"
-
-func main() {
-	app.StartApp()
-}
