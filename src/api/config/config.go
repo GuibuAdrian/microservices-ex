510576b002eb8ec0ea74133bdac985bef94baf3d
GuibuAdrian <guibuadrian@gmail.com>
2022-07-13 16:06:01 -0300

    Added integration & unit test

--- src/api/config/config.go
+++ src/api/config/config.go
@@ -2,9 +2,9 @@ package config
 
 import "os"
 
-const apiGithubAccessToken = "SECRET_GITHUB_ACCESS_TOKEN"
+const secretGithubAccessToken = "SECRET_GITHUB_ACCESS_TOKEN"
 
-var gihubAccessToken = os.Getenv(apiGithubAccessToken)
+var gihubAccessToken = os.Getenv(secretGithubAccessToken)
 
 func GetGitHubAccessToken() string {
 	return gihubAccessToken
